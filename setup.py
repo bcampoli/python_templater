7a765202e39a79d2f5db7ae608e71faf3c53fda8
Billy Campoli <william.campoli@logimethods.com>
2017-09-13 10:47:19 -0400

    added dependencies to setup file

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from distutils.core import setup
 setup(name='templater',
       version='1.0',
       py_modules=['templater'],
+      install_requires=[
+          'yaml',
+          're',
+      ],
       )
